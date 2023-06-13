f8088508928807c0f799c41debbd72caba68f2f0
manttus <rbasnyat666@gmail.com>
2023-06-13 15:55:49 +0545

    refactor: add new constants

--- server/constants/enums/enums.ts
+++ server/constants/enums/enums.ts
@@ -2,4 +2,14 @@ export enum FieldsEnum {
   Email = "Email",
   Password = "Password",
   Others = "Others",
+  Nullable = "Nullable",
+}
+
+export enum ResponseEnum {
+  Invalid_Cred = "Invalid Credentials",
+  Token_Expired = "Token Expired",
+  Invalid_Token = "Invalid Token",
+  Server_Error = "Server Error",
+  UnAuthorized = "UnAuthorized",
+  Created = "Created",
 }
