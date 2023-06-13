7339eec2716ceb592e10ce70d91a99fd621fab5a
manttus <rbasnyat666@gmail.com>
2023-06-13 16:00:59 +0545

    chore: delete unused file

--- server/application/use_cases/auth.ts
+++
@@ -1,8 +0,0 @@
-import { MongooseType } from "../../types/mongo.types";
-
-export const login = (
-  email: string,
-  password: string,
-  mongoose: MongooseType,
-  authService: Function
-) => {};
