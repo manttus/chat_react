840106c6126d7b001799db2318a234c2063d5e67
manttus <rbasnyat666@gmail.com>
2023-06-13 15:53:57 +0545

    refactor: update config for token

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,6 +1,8 @@
 const mongo_url = process.env.MONGO_URL;
 const port = process.env.PORT;
 const socket = process.env.SOCKET;
+const secret = process.env.SECRET;
+const refresh = process.env.REFRESH;
 
 export const config = {
   port: port || 3000,
@@ -9,4 +11,6 @@ export const config = {
     strictMode: true,
   },
   socket: socket || 3001,
+  secret: secret || "GGG",
+  refresh: refresh || "FFF",
 };
