d03c4642cbb0ca97ac0d9121d0533a44dc1bee09
manttus <rbasnyat666@gmail.com>
2023-06-15 16:33:04 +0545

    feat: update & delete user func

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -20,8 +20,57 @@ const userController = () => {
       return res.status(500).send({ message: ResponseEnum.Server_Error });
     }
   };
-  const updateUser = (req: RequestType, res: ResponseType, _: NextType) => {};
-  const deleteUser = (req: RequestType, res: ResponseType, _: NextType) => {};
+  const updateUser = async (
+    req: RequestType,
+    res: ResponseType,
+    _: NextType
+  ) => {
+    const error = validationResult(req);
+    if (!error.isEmpty())
+      return res
+        .status(400)
+        .send({ message: ResponseEnum.Invalid_Cred, error: error.array()[0] });
+    try {
+      const update = {
+        username: req.body.username,
+        image: req.body.image,
+      };
+      const isExist = await user.findOneAndUpdate(
+        { email: req.body.email },
+        update
+      );
+      if (!isExist)
+        return res.status(400).send({
+          message: ResponseEnum.Invalid_Cred,
+        });
+      return res.status(200).send({ message: "User Updated" });
+    } catch (err: Error | unknown) {
+      return res.status(500).send({ message: ResponseEnum.Server_Error });
+    }
+  };
+  const deleteUser = async (
+    req: RequestType,
+    res: ResponseType,
+    _: NextType
+  ) => {
+    const update = {
+      username: "user",
+      email: "user",
+      password: "user",
+      image: "user",
+    };
+    try {
+      const isExist = await user.findOneAndUpdate(
+        { email: req.body.email },
+        update
+      );
+      if (!isExist)
+        return res.status(400).send({ message: ResponseEnum.Invalid_Cred });
+      return res.status(200).send({ message: "User Removed" });
+    } catch (err: Error | unknown) {
+      return res.status(500).send({ message: ResponseEnum.Server_Error });
+    }
+  };
   return {
     profileUser,
     updateUser,
