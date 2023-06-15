98cab81573dafcbae50664be52cf3bbfec667793
manttus <rbasnyat666@gmail.com>
2023-06-15 15:18:03 +0545

    refactor: add otp & user route

--- server/frameworks/webserver/routes/index.ts
+++ server/frameworks/webserver/routes/index.ts
@@ -1,9 +1,13 @@
 import { baseURL } from "../../../constants/baseURL";
 import { AppType, ExpressType } from "../../../types/express";
 import authRouter from "./auth";
+import otpRouter from "./otp";
+import userRouter from "./user";
 
 const routes = (app: AppType, express: ExpressType) => {
   app.use(`${baseURL}auth`, authRouter(express));
+  app.use(`${baseURL}otp`, otpRouter(express));
+  app.use(`${baseURL}user`, userRouter(express));
 };
 
 export default routes;
