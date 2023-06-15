ad294ed2427cc00842b61c805cb825314710d516
manttus <rbasnyat666@gmail.com>
2023-06-15 15:15:50 +0545

    refactor: create status enum & add enum constants

--- server/constants/enums/enums.ts
+++ server/constants/enums/enums.ts
@@ -3,13 +3,24 @@ export enum FieldsEnum {
   Password = "Password",
   Others = "Others",
   Nullable = "Nullable",
+  OTP = "OTP",
 }
 
 export enum ResponseEnum {
+  Created = "Created",
   Invalid_Cred = "Invalid Credentials",
   Token_Expired = "Token Expired",
   Invalid_Token = "Invalid Token",
   Server_Error = "Server Error",
   UnAuthorized = "UnAuthorized",
-  Created = "Created",
+  Email_Sent = "Email Sent",
+  Email_Already = "Email Already Sent",
+  Invalid_OTP = "Invalid OTP",
+  Email_Verified = "Email Verified",
+}
+
+export enum Status {
+  PENDING = "Pending",
+  VERIFIED = "Verified",
+  EXPIRED = "Expired",
 }
