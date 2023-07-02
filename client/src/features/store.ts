c3974b11d87897e9fa41411d3387360ade0aa9f9
manttus <rbasnyat666@gmail.com>
2023-07-02 17:41:45 +0545

    feat: create global store

--- client/src/features/store.ts
+++ client/src/features/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice/authSlice";
+import messageReducer from "./slices/messageSlice/messageSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    message: messageReducer,
   },
 });
 
