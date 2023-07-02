ae50e85214c96ebf7e2d4315ba6d04105ec7272a
manttus <rbasnyat666@gmail.com>
2023-07-02 17:43:49 +0545

    refactor: complete & update searchtile

--- client/src/components/modals/components/SearchTile.tsx
+++ client/src/components/modals/components/SearchTile.tsx
@@ -1,5 +1,21 @@
-const SearchTile = () => {
-  return <div className="flex h-20 shadow-md"> </div>;
-}
+import Avatar from "../../avatar/Avatar";
+
+type SearchTileProps = {
+  image: string;
+  name: string;
+  onClick: () => void;
+};
+
+const SearchTile = ({ image, name, onClick }: SearchTileProps) => {
+  return (
+    <div
+      className="flex w-full gap-2 py-10 px-4 border border-gray-300 rounded-md items-center h-10 shadow-sm cursor-pointer"
+      onClick={onClick}
+    >
+      <Avatar image={image} />
+      {name}
+    </div>
+  );
+};
 
 export default SearchTile;
