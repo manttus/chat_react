d56844df299c28997dd7d5b25666f4e763244aa6
manttus <rbasnyat666@gmail.com>
2023-07-02 17:41:14 +0545

    refactor: update width form

--- client/src/components/form/CustomForm.tsx
+++ client/src/components/form/CustomForm.tsx
@@ -2,14 +2,17 @@ import { FormEvent, PropsWithChildren } from "react";
 
 type CustomFormProps = {
   onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+  width?: string;
 };
 
 const CustomForm = ({
   onSubmit,
+  width,
   children,
 }: PropsWithChildren<CustomFormProps>) => {
+  const style = `flex flex-col ${width ? width : "w-full"} gap-4`;
   return (
-    <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+    <form className={style} onSubmit={onSubmit}>
       {children}
     </form>
   );
